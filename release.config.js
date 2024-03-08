e53e6b6de2c590600690bad03e6f874b09f7b186
Mulraj Gupta <mulrajgupta208@gmail.com>
2024-03-08 10:07:57 +0530

    PKS-111 refactor: update release.config.js

--- release.config.js
+++ release.config.js
@@ -1,17 +1,21 @@
 module.exports = {
     branches: ['main'],
     "plugins": [
-        "@semantic-release/commit-analyzer",
-        ["semantic-release-jira-notes", {
-            "jiraHost": "aiopsgroup.atlassian.net",
-            "ticketPrefixes": ["PKS"]
-        }],
+        ["@semantic-release/commit-analyzer",
+            {
+                "preset": "angular",
+                "releaseRules": [
+                    {"type": "docs", "release": "patch"},
+                    {"type": "refactor", "release": "patch"},
+                    {"type": "style", "release": "patch"}
+                ],
+                "parserOpts": {
+                    headerPattern: /^([A-Z]+-\d+) (\w*)(?:\(([^)]*)\))?: (.*)$/,
+                    headerCorrespondence: ['jiraId', 'type', 'scope', 'subject']
+                }
+            }
+        ],
         "@semantic-release/git",
-        "@semantic-release/github",
-        ['@digitalroute/cz-conventional-changelog-for-jira', {
-            "jiraLocation": "pre-type",
-            "skipScope": false,
-            "CZ_JIRA_PREFIX": "PKS"
-        }]
+        "@semantic-release/github"
     ],
 }
