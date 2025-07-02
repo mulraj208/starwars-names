61778371c69900c09347bedf5deb96ce309a347b
Mulraj Gupta <mulrajgupta208@gmail.com>
2025-07-02 15:21:48 +0530

    PKS-68 chore: remove the `dry-run` from release command

--- conventional-commits-parser.config.js
+++ conventional-commits-parser.config.js
@@ -1,5 +1,6 @@
 // Documentation for the options
 // https://github.com/release-it/conventional-changelog?tab=readme-ov-file#parseropts
+
 const jiraUrl = 'https://aiopsgroup.atlassian.net/browse/'
 
 module.exports = {
@@ -38,54 +39,73 @@ module.exports = {
     groupBy: 'type',
     noteGroupsSort: 'scope',
     commitPartial:
-      `*{{#if scope}} **{{scope}}:** {{~/if}} {{#if subject}}
-          {{~subject}} 
-      {{~else}}
-          {{~header}} 
-      {{~/if}}
-      
-      {{~!-- commit link --}}
-      {{#if @root.linkReferences~}}
-          ([{{hash}}](
-          {{~#if @root.repository}}
-              {{~#if @root.host}}{{@root.host}}/{{/if}}
-              {{~#if @root.owner}}{{@root.owner}}/{{/if}}
-              {{~@root.repository}}/commit/{{hash}}
-          {{~else}}
-              {{~@root.repoUrl}}/commit/{{hash}}
-          {{~/if}})) 
-      {{~else}}
-          {{~hash}}
-      {{~/if}}
-      
-      {{~!-- Jira link --}}
-      {{#if jiraId}}
-          ([{{ jiraId }}](${jiraUrl}{{ jiraId }}))
-      {{/if}}
-      
-      {{~!-- commit references --}}
-      {{~#if references~}}
-          , closes
-          {{~#each references}} {{#if @root.linkReferences~}}
-              [
-              {{~#if this.owner}}{{this.owner}}/{{/if}}
-              {{this.repository}}#{{this.issue}}](
-              {{~#if @root.repository}}
-                  {{~#if @root.host}}{{@root.host}}/{{/if}}
-                  {{~#if this.repository}}
-                      {{~#if this.owner}}{{this.owner}}/{{/if}}
-                      {{this.repository}}
-                  {{~else}}
-                      {{~#if @root.owner}}{{@root.owner}}/{{/if}}
-                      {{@root.repository}}
-                  {{~/if}}
-              {{~else}}
-                  {{@root.repoUrl}}
-              {{~/if}}/{{@root.issue}}/{{this.issue}})
-          {{~else}}
-              {{~#if this.owner}}{{this.owner}}/{{/if}}
-              {{this.repository}}#{{this.issue}}
-          {{~/if}}{{/each}}
-      {{~/if}}`
+      '*{{#if scope}} **{{scope}}:**\n' +
+      '{{~/if}} {{#if subject}}\n' +
+      '    {{~subject}}\n' +
+      '{{~else}}\n' +
+      '    {{~header}}\n' +
+      '{{~/if}}\n' +
+      '\n' +
+      '{{~!-- commit link --}}\n' +
+      '{{#if @root.linkReferences~}}\n' +
+      '       ([{{shortHash}}](\n' +
+      '    {{~#if @root.repository}}\n' +
+      '        {{~#if @root.host}}\n' +
+      '            {{~@root.host}}/\n' +
+      '        {{~/if}}\n' +
+      '        {{~#if @root.owner}}\n' +
+      '            {{~@root.owner}}/\n' +
+      '        {{~/if}}\n' +
+      '        {{~@root.repository}}\n' +
+      '    {{~else}}\n' +
+      '        {{~@root.repoUrl}}\n' +
+      '    {{~/if}}/\n' +
+      '    {{~@root.commit}}/{{hash}}))\n' +
+      '{{~else}}\n' +
+      '    {{~shortHash}}\n' +
+      '{{~/if}}\n' +
+      '\n' +
+      '{{~!-- Jira link --}}\n' +
+      '{{#if jiraId}}\n' +
+      '    ([{{ jiraId }}](' +
+      jiraUrl +
+      '{{ jiraId }}))\n' +
+      '{{/if}}\n' +
+      '\n' +
+      '{{~!-- commit references --}}\n' +
+      '{{~#if references~}}\n' +
+      '    , closes\n' +
+      '    {{~#each references}} {{#if @root.linkReferences~}}\n' +
+      '        [\n' +
+      '        {{~#if this.owner}}\n' +
+      '            {{~this.owner}}/\n' +
+      '        {{~/if}}\n' +
+      '        {{~this.repository}}#{{this.issue}}](\n' +
+      '        {{~#if @root.repository}}\n' +
+      '            {{~#if @root.host}}\n' +
+      '                {{~@root.host}}/\n' +
+      '            {{~/if}}\n' +
+      '            {{~#if this.repository}}\n' +
+      '                {{~#if this.owner}}\n' +
+      '                    {{~this.owner}}/\n' +
+      '                {{~/if}}\n' +
+      '                {{~this.repository}}\n' +
+      '            {{~else}}\n' +
+      '                {{~#if @root.owner}}\n' +
+      '                    {{~@root.owner}}/\n' +
+      '                {{~/if}}\n' +
+      '                {{~@root.repository}}\n' +
+      '            {{~/if}}\n' +
+      '        {{~else}}\n' +
+      '            {{~@root.repoUrl}}\n' +
+      '        {{~/if}}/\n' +
+      '        {{~@root.issue}}/{{this.issue}})\n' +
+      '    {{~else}}\n' +
+      '        {{~#if this.owner}}\n' +
+      '            {{~this.owner}}/\n' +
+      '        {{~/if}}\n' +
+      '        {{~this.repository}}#{{this.issue}}\n' +
+      '    {{~/if}}{{/each}}\n' +
+      '{{~/if}}\n'
   }
 }
